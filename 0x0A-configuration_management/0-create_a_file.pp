3a33b491e1c328583f6d1db9d2178ded41c569f6
nyangoto <patricknyangoto@hotmail.com>
2021-08-13 22:17:21 +0000

    Linter comliance

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file /tmp/holberton
-file { 'holberton':
-	ensure 	=> '/tmp/holberton',
-	path 	=> '/tmp/holberton',
-	mode 	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content => 'I love puppet',
+file { '/tmp/holberton':
+ensure   => file
+  path	 => '/tmp/holberton',
+  mode 	 => '0744',
+  owner	 => 'www-data',
+  group	 => 'www-data',
+content  => 'I love puppet'
 }
