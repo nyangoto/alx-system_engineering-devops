a6742ad5dc48bddac2bea9f123fb41b4f2c5ff76
nyangoto <patricknyangoto@hotmail.com>
2021-08-13 21:35:51 +0000

    Syntax and Linter compliance

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file /tmp/holberton
-file { 'holberton':
-	ensure => "/tmp/holberton",
-	content => "I love puppet",
-	mode => "0744",
-	group => "www-data",
-	owner => "www-data",
+file { '/tmp/holberton':
+	ensure 	=> 'file',
+	path 	=> '/tmp/holberton',
+	mode 	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	content => 'I love puppet',
 }
