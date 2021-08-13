7c1f6dba3d8a18c97627c1bc7ea49d029cd3fedc
nyangoto <patricknyangoto@hotmail.com>
2021-08-13 22:23:23 +0000

    Try fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file /tmp/holberton
 file { '/tmp/holberton':
-ensure   => file
-  path	 => '/tmp/holberton',
-  mode 	 => '0744',
-  owner	 => 'www-data',
-  group	 => 'www-data',
-content  => 'I love puppet'
+  ensure  => file,
+  path	  => '/tmp/holberton',
+  mode 	  => '0744',
+  owner	  => 'www-data',
+  group	  => 'www-data',
+  content => 'I love puppet'
 }
