a9915c352075d97565ec2f70d32fc3e0e67de131
nyangoto <patricknyangoto@hotmail.com>
2021-08-13 21:30:53 +0000

    Fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# create a file /tmp/holberton
 file { 'holberton':
-	ensure => "/tmp/holberton"
-		content => "I love puppet"
-		mode => "0744"
-		group => "www-data"
-		owner => "www-data"
+	ensure => "/tmp/holberton",
+	content => "I love puppet",
+	mode => "0744",
+	group => "www-data",
+	owner => "www-data",
 }
