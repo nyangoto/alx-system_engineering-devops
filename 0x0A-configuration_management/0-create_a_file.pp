94c42b83a8e109125d1b392e8eece6af438465b6
nyangoto <patricknyangoto@hotmail.com>
2021-08-13 22:35:16 +0000

    cdhzj

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file /tmp/holberton
 file { '/tmp/holberton':
   ensure  => file,
-  mode 	  => '0744',
-  owner	  => 'www-data',
-  group	  => 'www-data',
-  content => 'I love puppet'
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
